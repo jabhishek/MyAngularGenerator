8734677f4057e976a0e4fde39e5241e641a60a8e
jain208 <abhi2000@gmail.com>
2014-05-08 17:27:22 +0100

    fixed api url in notes api

--- server/services/notes.js
+++ server/services/notes.js
@@ -1,7 +1,7 @@
 (function (notesApi) {
 	console.log("notes.js loaded");
     notesApi.init = function (router) {
-        router.get('/', function(req, res) {
+        router.get('/notes', function(req, res) {
 		    res.json({ message: 'hooray! welcome to my api!' });   
 		});
     };
