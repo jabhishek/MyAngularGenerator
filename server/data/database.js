df0d8de57e5a1b8a5c6708383c085537292a04c1
jain208 <abhi2000@gmail.com>
2014-05-13 10:42:40 +0100

    changed database.js to use mongolab - retrieve connectionstring from dbconfig

--- server/data/database.js
+++ server/data/database.js
@@ -1,8 +1,9 @@
 (function (database) {
     var mongodb = require("mongodb");
+    var connection = require("./connection");
     // theBoard is the name of the database. 
     // If mongoDb doesn't find the database, it will create it for you.
-    var mongoUrl = "mongodb://localhost:27017/theBoard";
+    var mongoUrl = connection.connectionString;
     var theDb = null;
     database.getDb = function(next) {
         if (!theDb) {
